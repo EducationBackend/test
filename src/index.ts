a04c45dacad6aea5bcbb5682b42a8c125be29638
vgodun <vgodunsoftware@gmail.com>
2024-07-26 02:47:55 +0300

    fix vs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const greet = (name: string) => `Hello, ${name}!!!`;
+export const greet = (name: string) => `H!ello, ${name}!!!`;
 
 export const greetName = (name: string) => `Hello, ${name}!!!!`;
